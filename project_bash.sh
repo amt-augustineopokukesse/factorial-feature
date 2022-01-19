ff3456fc88c7931666dab7fb92d211c35d51115c
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.org>
2022-01-19 10:50:47 +0100

    Write initial test(Red part1)

---
+++ project_bash.sh
@@ -0,0 +1,26 @@
+# The RED-GREEN-Refactor Cycle
+# Red Part 1a and 1b
+$ npm test
+
+> blank-workspace@1.0.0 test /home/ccuser/workspace/tdd-mocha-project
+> mocha
+
+
+
+  Calculate
+    .factorial
+      1) Tests if the output of 5! is equal to 120
+
+
+  0 passing (6ms)
+  1 failing
+
+  1) Calculate .factorial Tests if the output of 5! is equal to 120:
+     TypeError: Calculate.factorial is not a function
+      at Context.it (test/index_test.js:7:30)
+
+
+
+npm ERR! Test failed.  See above for more details.
+
+
--- test/index_test.js
+++ test/index_test.js
@@ -3,6 +3,8 @@ var Calculate =  require('../index.js')
 
 describe('Calculate', () => {
   describe('.factorial', () => {
-    
+    it('Tests if the output of 5! is equal to 120', () => {
+      assert.equal(Calculate.factorial(5), 120);
+    })
   });
 });
